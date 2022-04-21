b414aa0061126c56ad8f5b40433f4c5b11ba7c30
AleksandarIlicc <aleksandari336@gmail.com>
2022-04-22 00:48:46 +0200

    initial commit

---
+++ app.js
@@ -0,0 +1,37 @@
+'use strict';
+
+// const myName = 'Jonas';
+
+// function first() {
+//     let a = 1;
+//     const b = second();
+//     a = a + b;
+//     return a;
+// }
+
+// function second() {
+//     const c = 2;
+//     return c;
+// }
+
+// const x = first();
+
+// console.log(x);
+
+// const a = 'Jonas';
+// first();
+
+// function first() {
+//     const b = 'Hello';
+//     second();
+
+//     function second() {
+//         const c = 'Hi';
+//         third();
+//     }
+// }
+
+// function third() {
+//     const d = 'Hey';
+//     console.log(d + c + b + a);
+// }
